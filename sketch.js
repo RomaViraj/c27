1c570c79e632b7a9ccca03fcc6402610e89aeab6
RomaViraj <rashmita18jan@gmail.com>
2020-09-15 20:21:54 +0530

    add chain constraint

--- sketch.js
+++ sketch.js
@@ -1,10 +1,13 @@
 const Engine = Matter.Engine;
 const World= Matter.World;
 const Bodies = Matter.Bodies;
+const Constraint= Matter.Constraint;
 
 var engine, world;
 var box1, pig1;
 var backgroundImg,platform;
+//var constraintLog;
+var log6;
 
 function preload() {
     backgroundImg = loadImage("sprites/bg.png");
@@ -15,6 +18,8 @@ function setup(){
     engine = Engine.create();
     world = engine.world;
 
+   // constraintLog= new Log(230,180,80,PI/2);  
+   log6= new Log(230,180,80,PI/2);    
 
     ground = new Ground(600,height,1200,20);
     platform = new Ground(150, 305, 300, 170);
@@ -35,6 +40,17 @@ function setup(){
     log5 = new Log(870,120,150, -PI/7);
 
     bird = new Bird(100,100);
+    chain=new Chain(bird.body,log6.body);
+
+   /* var options={
+        bodyA:bird.body,
+        bodyB:constraintLog.body,
+        stiffness:0.04,
+        length:10
+
+    }
+    var chain=Constraint.create(options);
+    World.add(world,chain);*/
 
 }
 
@@ -61,4 +77,11 @@ function draw(){
 
     bird.display();
     platform.display();
+   // constraintLog.display();
+
+   // strokeWeight(5);
+    //line(bird.body.position.x,bird.body.position.y,constraintLog.body.position.x,constraintLog.body.position.y);
+
+    log6.display();
+    chain.display();
 }
